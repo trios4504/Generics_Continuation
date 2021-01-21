3472ba21d8510e2f37923e1a6ba16f75d8f9bb44
trios4504 <67927315+trios4504@users.noreply.github.com>
2021-01-20 23:04:33 -0700

    Add files via upload

---
+++ Generics_Continuation/Generics_Continuation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Generics_Continuation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cake<string> weddingCake = new Cake<string>();
+            weddingCake.Flavor = "Vanilla";
+
+            Cake<string> partyCake = new Cake<string>();
+            partyCake.Flavor = "Chocolate";
+
+            Console.WriteLine("The flavor for the Wedding cake is: " + weddingCake.Flavor + ".");
+            Console.WriteLine("The flavor for the Party cake is: " + partyCake.Flavor + ".");
+
+        }
+    }
+}
+
+public class Cake<T>
+{
+    public string Flavor { get; set; }
+}
